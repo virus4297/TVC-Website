cfe529a8fe5ba2a7062e1504d23c93075ece8b29
Viral Ashok Panchal <vpan0015@student.monash.edu>
2025-07-10 21:47:43 +1000

    change to static simple page

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { generateClient } from 'aws-amplify/data';
-import type { Schema } from '../../../amplify/data/resource';
 
-const client = generateClient<Schema>();
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
@@ -12,45 +9,4 @@ const client = generateClient<Schema>();
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css',
 })
-export class TodosComponent implements OnInit {
-  todos: any[] = [];
-
-  ngOnInit(): void {
-    this.listTodos();
-  }
-
-  deleteTodo(id: string) {
-    try {
-      if (confirm('Are you sure you want to delete this todo?')) {
-        client.models.Todo.delete({ id });
-        this.todos = this.todos.filter(todo => todo.id !== id);
-      }
-    } catch (error) {
-      console.error('error deleting todo', error);
-    }
-  }
-
-
-  listTodos() {
-    try {
-      client.models.Todo.observeQuery().subscribe({
-        next: ({ items, isSynced }) => {
-          this.todos = items;
-        },
-      });
-    } catch (error) {
-      console.error('error fetching todos', error);
-    }
-  }
-
-  createTodo() {
-    try {
-      client.models.Todo.create({
-        content: window.prompt('Todo content'),
-      });
-      this.listTodos();
-    } catch (error) {
-      console.error('error creating todos', error);
-    }
-  }
-}
+export class TodosComponent {}
