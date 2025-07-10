0eba1251f4e392cacd6c47cf7fe6c23bde79ad5c
Viral Ashok Panchal <vpan0015@student.monash.edu>
2025-07-10 23:15:48 +1000

    developer page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./todos/todos.component').then(m => m.TodosComponent) },
+  { path: 'developer', loadComponent: () => import('./developer/developer.component').then(m => m.DeveloperComponent) },
+  { path: '**', redirectTo: '' }
+];
