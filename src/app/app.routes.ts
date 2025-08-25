02f118cd26593c591fdfd50271aece6cd555864e
Viral Panchal <viral1997@gmail.com>
2025-08-26 00:09:34 +1000

    what is digital media

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LetslearnscratchComponent } from './letslearnscratch/letslearnscratch.component';
+import { DigitalMarketingComponent } from './digital-marketing/digital-marketing.component';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./todos/todos.component').then(m => m.TodosComponent) },
   { path: 'developer', loadComponent: () => import('./developer/developer.component').then(m => m.DeveloperComponent) },
   { path: 'letslearnscratch', component: LetslearnscratchComponent },
+  { path: 'digital-marketing', component: DigitalMarketingComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/digital-marketing/digital-marketing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
+
+@Component({
+  selector: 'app-digital-marketing',
+  standalone: true,
+  imports: [NavComponent],
+  templateUrl: './digital-marketing.component.html',
+  styleUrl: './digital-marketing.component.css'
+})
+export class DigitalMarketingComponent {
+
+}
